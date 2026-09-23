5966f2933749f7f6d271a6e47ac96ac40e410898
Anushree-cod <anushreeskabra@gmail.com>
2026-09-23 13:42:24 +0530

    Time: 2 ms (38.25%) | Memory: 13.3 MB (87.63%) - LeetSync

---
+++ 137-single-number-ii/single-number-ii.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+       int ans = 0;
+        for(int i = 0; i< 32; i++){
+            int count = 0;
+            for(int num = 0; num < nums.size(); num++){
+                if(nums[num] & (1 << i)){
+                    count++;
+                }
+            }
+        
+        if(count % 3 != 0){
+            ans = ans | (1 << i);
+        }
+        }
+        return ans;
+    }
+};
